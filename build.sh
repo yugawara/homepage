5cbefdc1cd55ccf4aaa903da7b9af842ade7ca34
Yasuaki Kudo <yasu@yasuaki.com>
2022-05-05 21:21:06 +0900

    build.sh fix

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 pwd
-python3 -m venv ve
-source ve/bin/activate
-python3 -m pip install "pelican[markdown]"
+#python3 -m venv ve
+#source ve/bin/activate
+#python3 -m pip install "pelican[markdown]"
 pushd retrosynth
 pelican -s pelicanconf.py -o output content
 #pelican -s pelicanconf.py -o output -t theme content
