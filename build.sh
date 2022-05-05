3604653201c98b9e784f398a2de234652c2976d9
Yasuaki Kudo <yasu@yasuaki.com>
2022-05-05 21:13:13 +0900

    build.sh fix

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ cd $SCRIPT_DIR
 pwd
 python3 -m venv ve
 source ve/bin/activate
-python -m pip install "pelican[markdown]"
+python3 -m pip install "pelican[markdown]"
 pushd retrosynth
 pelican -s pelicanconf.py -o output content
 #pelican -s pelicanconf.py -o output -t theme content
