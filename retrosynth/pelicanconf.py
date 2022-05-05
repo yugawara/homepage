9e311a420cdae7d381746c43e2292ab699a2061a
Yasuaki Kudo <yasu@yasuaki.com>
2022-05-05 19:42:53 +0900

    following the steps

---
+++ retrosynth/pelicanconf.py
@@ -0,0 +1,31 @@
+AUTHOR = 'YasuKudo'
+SITENAME = 'RetroSynthTitle'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Tokyo'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('Full Stack Python', 'https://www.fullstackpython.com/'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
